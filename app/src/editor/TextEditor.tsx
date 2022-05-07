2c3833c9051c507fd518cea2b0d7dc6d019a527c
Robin Thöne <>
2022-05-07 10:10:12 +0200

    fix formatting

--- app/src/editor/TextEditor.tsx
+++ app/src/editor/TextEditor.tsx
@@ -61,8 +61,8 @@ export const TextEditor: FunctionComponent<ITextEditor> = (props) => {
         props.initialContent && props.contentType === 'markdown'
             ? getEditorStateFromMarkdown(props.initialContent)
             : props.initialContent && props.contentType === 'html'
-            ? getEditorStateFromHtml(props.initialContent)
-            : EditorState.createEmpty()
+                ? getEditorStateFromHtml(props.initialContent)
+                : EditorState.createEmpty()
     );
 
     /** The currently selected heading type. */
